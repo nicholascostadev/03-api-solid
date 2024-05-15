0bd8dcfef290b646b2a12cb3289171f00e4a5b3b
Nicholas Costa <nicholascostadev@gmail.com>
2024-05-15 01:07:13 -0300

    feat: add first create user route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import { fastify } from 'fastify'
+import { z } from 'zod'
+import { db } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const RegisterBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = RegisterBodySchema.parse(request.body)
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password, // TODO: hash password
+    },
+  })
+
+  return reply.status(201).send()
+})
