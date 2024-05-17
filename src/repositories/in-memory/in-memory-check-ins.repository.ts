55cb44431cf4de3db794790f8b9640d7eef95013
Nicholas Costa <nicholascostadev@gmail.com>
2024-05-17 14:25:06 -0300

    feat: add validate check-in use-case

--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -41,9 +41,27 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return isCheckInOnSameDate
   }
 
+  async findById(id: string) {
+    const checkIn = this.items.find((checkIn) => checkIn.id === id)
+
+    if (!checkIn) return null
+
+    return checkIn
+  }
+
   async findManyByUserId(userId: string, page: number) {
     return this.items
       .filter((checkIn) => checkIn.user_id === userId)
       .slice((page - 1) * 20, page * 20)
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((c) => c.id === checkIn.id)
+
+    if (checkInIndex !== -1) {
+      this.items[checkInIndex] = checkIn
+    }
+
+    return checkIn
   }
 }
