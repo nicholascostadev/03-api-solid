bfa14a5c242fd5d03281b62094482674cfbbc24e
Nicholas Costa <nicholascostadev@gmail.com>
2024-05-23 00:50:43 -0300

    feat: add refresh token logic

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { profileController } from './profile.controller'
 import { registerController } from './register.controller'
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../../middlewares/verify-jwt'
+import { refreshController } from './refresh.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', authenticateController)
+
+  app.patch('/token/refresh', refreshController)
 
   /* Authenticated Routes */
   app.get(
